eb6e0141c54ad668819b415b6ce56e3a9387dadf
Achala Bandara <hmapb1994@gmail.com>
2020-05-21 22:53:55 +0530

    second commit

---
+++ canvas.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+
+  //Resizing
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  //variables
+  let painting = false;
+
+  function startPosition(e) {
+    painting = true;
+    draw(e);
+  }
+
+  function finishedPosition() {
+    painting = false;
+    ctx.beginPath();
+  }
+
+  //EventListeners
+  function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+  }
+
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
+});
